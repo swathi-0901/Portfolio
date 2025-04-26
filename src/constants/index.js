ba01351f75aa61592a0237cd621b7e0d6a335a9b
swathi-0901 <swathi.kasikala@gmail.com>
2025-04-26 13:54:11 -0400

    new addition

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,9 +18,9 @@ export const ABOUT_TEXT = `I am a dedicated and versatile web developer with a s
 export const EXPERIENCES = [
   {
     year: "February 2025 - Present",
-    role: "Web Specialist",
+    role: "Web and Data Administrator",
     company: "Jade Scientific",
-    description: `As a Web Specialist, I develop and optimize WordPress websites using PHP, HTML5, CSS3, JavaScript, and frameworks like React and Vue.js, focusing on performance, SEO, accessibility, and cross-browser compatibility. I implement custom themes, plugins, and integrations, streamline workflows with tools like Webpack, npm, and Git, and collaborate with marketing teams to align web solutions with business goals. Additionally, I manage NetSuite ERP data, ensuring accuracy and consistency, while conducting testing and debugging to ensure website reliability, security, and compliance.`,
+    description: `I develop and optimize WordPress websites using PHP, HTML5, CSS3, JavaScript, and frameworks like React and Vue.js, focusing on performance, SEO, accessibility, and cross-browser compatibility. I implement custom themes, plugins, and integrations, streamline workflows with tools like Webpack, npm, and Git, and collaborate with marketing teams to align web solutions with business goals. Additionally, I manage NetSuite ERP data, ensuring accuracy and consistency, while conducting testing and debugging to ensure website reliability, security, and compliance.`,
     technologies: ["React.js", "NetSuite", "Vue.js", "HTML","CSS", "JavaScript", "Wordpress"],
   },
   {
